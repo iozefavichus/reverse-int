c9c8a119ee25439daf80229e4d3d7ee1cd03b4fd
iozefavichus <iozefavichus@gmail.com>
2023-01-08 21:14:46 +0200

    fix: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let absN = Math.abs(n);
+    let stringN = '' + absN;
+    let stringSplit = stringN.split("");
+    let reversSplit = stringSplit.reverse();
+    let resultStr = reversSplit.join("");
+    let result = + resultStr;
+    return result;
 }
